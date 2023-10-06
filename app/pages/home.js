b89e6c99bfe925e196ac3aa9c863dcfd9d944ca1
wesleyRenam <wesleyrenamjob@gmail.com>
2023-10-05 23:28:52 -0300

    adicionando relatorios

--- app/pages/home.js
+++ app/pages/home.js
@@ -1,33 +1,50 @@
 'use client';
 import { FaCashRegister } from 'react-icons/fa';
 import SideBar from '../components/sidebar'
+import {useState} from 'react';
+import useInterval from "../useInterval";
 import { BsCashStack, BsSearch, BsPlus } from "react-icons/bs";
 
 export default function Login() {
+
+  const [dataHora, setDataHora] = useState(new Date());
+
+  useInterval(() => {
+    setDataHora(new Date());
+  }, 1000);
+
+  const formatoDataHora = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
 
   return (
     <main className='md:flex justify-center items-center'>
       <SideBar />
       <div className='flex-col justify-center items-center'>
         <div className='h-full grid content-center justify-items-center gap-2 xl:gap-6 w-full mt-2 grid-cols-2 lg:grid-cols-4 lg:scale-1.5'>
-          <a href="#" class="grid text-gray-900 gap-1 content-center justify-items-center rounded-lg dark:text-white group w-36 h-36 xl:w-72 xl:h-72 md:w-44 md:h-44 border-2 border-gray-800 p-1 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700">
-            <BsCashStack class="w-8 h-8 text-white">
+          <a href="#" className="grid text-gray-900 gap-1 content-center justify-items-center rounded-lg dark:text-white group w-36 h-36 xl:w-72 xl:h-72 md:w-44 md:h-44 border-2 border-gray-800 p-1 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700">
+            <BsCashStack className="w-8 h-8 text-white">
             </BsCashStack>
             <span className="text-rose-600 text-xl ">VENDER</span>
           </a>
-          <a href="#" class="grid text-gray-900 gap-1 content-center justify-items-center rounded-lg dark:text-white group w-36 h-36 xl:w-72 xl:h-72 md:w-44 md:h-44 border-2 border-gray-800 p-1 dark:text-gray-400 group-hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700">
-            <BsSearch class="w-8 h-8 text-white"></BsSearch>
-            <span class="text-xl">PESQUISAR</span>
+          <a href="#" className="grid text-gray-900 gap-1 content-center justify-items-center rounded-lg dark:text-white group w-36 h-36 xl:w-72 xl:h-72 md:w-44 md:h-44 border-2 border-gray-800 p-1 dark:text-gray-400 group-hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700">
+            <BsSearch className="w-8 h-8 text-white"></BsSearch>
+            <span className="text-xl">PESQUISAR</span>
           </a>
-          <a href="#" class="grid text-gray-900 gap-1 content-center justify-items-center rounded-lg dark:text-white group w-36 h-36 xl:w-72 xl:h-72 md:w-44 md:h-44 border-2 border-gray-800 p-1 dark:text-gray-400 group-hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700">
-            <FaCashRegister class="w-8 h-8 text-white">
+          <a href="#" className="grid text-gray-900 gap-1 content-center justify-items-center rounded-lg dark:text-white group w-36 h-36 xl:w-72 xl:h-72 md:w-44 md:h-44 border-2 border-gray-800 p-1 dark:text-gray-400 group-hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700">
+            <FaCashRegister className="w-8 h-8 text-white">
             </FaCashRegister>
-            <span class="text-3xl">CAIXA</span>
+            <span className="text-3xl">CAIXA</span>
           </a>
-          <a href="#" class="grid text-gray-900 gap-1 content-center justify-items-center rounded-lg dark:text-white group w-36 h-36 xl:w-72 xl:h-72 md:w-44 md:h-44 border-2 border-gray-800 p-1 dark:text-gray-400 group-hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700">
-            <BsPlus class="w-12 h-12 text-white">
+          <a href="#" className="grid text-gray-900 gap-1 content-center justify-items-center rounded-lg dark:text-white group w-36 h-36 xl:w-72 xl:h-72 md:w-44 md:h-44 border-2 border-gray-800 p-1 dark:text-gray-400 group-hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700">
+            <BsPlus className="w-12 h-12 text-white">
             </BsPlus>
-            <span class="text-xl text-emerald-500">ADICIONAR</span>
+            <span className="text-xl text-emerald-500">ADICIONAR</span>
           </a>
         </div>
         <div className='flex justify-center items-center'>
@@ -44,8 +61,11 @@ export default function Login() {
           <div className='flex justify-center items-center flex-col mx-4'>
             <h2>Faturamento</h2>
             <h2>R$ 25, 00</h2>
-          </div>
+          </div>         
         </div>
+        <footer className='flex justify-center items-center fixed bottom-0 w-full'>
+          <p>{dataHora.toLocaleDateString(undefined, formatoDataHora)}</p>
+        </footer>
       </div>
     </main>
   )
